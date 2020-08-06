1491a1d12bd60556768b3aca84890107ec643eaa
GorlovD <69232327+GorlovD@users.noreply.github.com>
2020-08-06 16:41:53 +0700

    Add files via upload

---
+++ Pagination/Pagination/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Pagination.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pagination.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Phone> phones;
+        public HomeController()
+        {
+            phones = new List<Phone>();
+            phones.Add(new Phone { Id = 0, Fam = "Иванов", Tel = "8923215634" });
+            phones.Add(new Phone { Id = 1, Fam = "Петров", Tel = "8923215734" });
+            phones.Add(new Phone { Id = 2, Fam = "Сидоров", Tel = "8929615634" });
+            phones.Add(new Phone { Id = 3, Fam = "Смирнов", Tel = "8923215364" });
+            phones.Add(new Phone { Id = 4, Fam = "Быков", Tel = "8923215619" });
+            phones.Add(new Phone { Id = 5, Fam = "Жданов", Tel = "8923217434" });
+            phones.Add(new Phone { Id = 6, Fam = "Кузнецов", Tel = "8923215619" });
+            phones.Add(new Phone { Id = 7, Fam = "Новоселов", Tel = "8923213126" });
+            phones.Add(new Phone { Id = 8, Fam = "Грушин", Tel = "8923273455" });
+            phones.Add(new Phone { Id = 9, Fam = "Семечев", Tel = "8923624977" });
+        }
+        public ActionResult Index(int page = 1)
+        {
+            int pageSize = 3; // количество объектов на страницу
+            IEnumerable<Phone> phonesPerPages = phones.Skip((page - 1) * pageSize).Take(pageSize);
+            PageInfo pageInfo = new PageInfo { PageNumber = page, PageSize = pageSize, TotalItems = phones.Count };
+            IndexViewModel ivm = new IndexViewModel { PageInfo = pageInfo, Phones = phonesPerPages };
+            return View(ivm);
+        }
+
+    }
+}
---
+++ Pagination/Pagination/Helpers/PaginationHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Web;
+using System.Text;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+
+namespace Pagination.Helpers
+{
+
+
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Fam { get; set; }
+        public string Tel { get; set; }
+    }
+
+
+    public class PageInfo
+    {
+
+        public int PageNumber { get; set; } // номер текущей страницы  
+        public int PageSize { get; set; } //  количество элементов на страницу
+
+        public int Element { get; set; }//   количество элементов на указанной странице
+
+        public int TotalItems { get; set; } //  количество элементов массива данных    
+       
+        public int TotalPages  //  количество доступных страниц
+        {
+            get { return (int)Math.Ceiling((decimal)TotalItems / PageSize); }
+        }
+    }
+    public class IndexViewModel
+    {
+        public IEnumerable<Phone> Phones { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+
+
+    public static class PaginationHelper
+    {
+        
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+            PageInfo pageInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+
+
+            for (int i = 1; i <= pageInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = (i-1).ToString();
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+
+    }
+
+}
